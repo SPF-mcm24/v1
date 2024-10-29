77c5463671fe9cb098387252a6c596e297be1126
SPF-mcm24 <168230656+SPF-mcm24@users.noreply.github.com>
2024-10-29 16:22:06 -0300

    Add files via upload

--- utils/script.js
+++ utils/script.js
@@ -1,20 +1,21 @@
 //script.js
 const pontos = [
-    { nome: "Condomel – Paço Municipal", endereco: "R. Leonardo Meca, 56 – Centro", lat: -23.7095169, lng: -46.412993 },
-    { nome: "Condomel – Câmara Municipal", endereco: "Rua Virgílio Gola, 40 – Pastoril", lat: -23.7112766, lng: -46.4112281 },
-    { nome: "Condomel – Espaço Lavoisier", endereco: "Av. Pref. Valdírio Prisco, 200 – Centro", lat: -23.7081625, lng: -46.4173833  },
-    { nome: "Condomel – Abelhas da Justiça", endereco: "Av. Pref. Valdírio Prisco, 150 – Centro", lat: -23.7083724, lng: -46.4175207 },
-    { nome: "Condomel – Colégio Gran Leone", endereco: "Jardim Panorama – R. Águida Tori Sortino, 90 – Centro", lat: -23.7075713, lng: -46.4087648 },
-    { nome: "Condomel – Abelhas da Saúde", endereco: "Rua Júlio Prestes 22, Jardim Luzo", lat: -23.7112517, lng: -46.38577 },
-    { nome: "Condomel – Pomar Urbano", endereco: "Estr. da Col., 1500 – Santa Luzia", lat: -23.6944009, lng: -46.3967345 },
-    { nome: "Condomel – LeBem", endereco: "Av. Benjamim Baptista Cerezoli, 580 – Pilar Velho", lat: -23.6821879, lng: -46.4074776 },
-    { nome: "Condomel – Abelhas da Educação", endereco: "R. Emerson Conde Soares Giacomini, 200 – Ouro Fino", lat: -23.6818131, lng: -46.355889 },
-    { nome: "Condomel – CRI", endereco: "R. Alferes Botacin, 171 – Centro Alto", lat: -23.7157106, lng: -46.4132153 },
-    { nome: "Condomel – Escola Municipal Edir Maria de Oliveira", endereco: "Av. Ver. Rubens Maziero, 526 – Sítio Santana", lat: -23.6747838, lng: -46.3452608 },
-    { nome: "Condomel – Toth", endereco: "R. Osvaldo Cruz, 02 – Centro", lat: -23.71965, lng: -46.4105132 },
-    { nome: "Condomel – Estancia Delfis", endereco: "R. Eugênio Galo, 145 – Centro", lat: -23.7067633, lng: -46.4084658 },
-    { nome: "Condomel – CAPS Infantil", endereco: "R. Primeiro de Maio, 108 – Jardim Itacolomy", lat: -23.706735, lng:-46.4167056 }
+    { nome: "Meliponário – Paço Municipal", endereco: "R. Leonardo Meca, 56 – Centro", lat: -23.7095169, lng: -46.412993 },
+    { nome: "Meliponário – Câmara Municipal", endereco: "Rua Virgílio Gola, 40 – Pastoril", lat: -23.7112766, lng: -46.4112281 },
+    { nome: "Meliponário – Espaço Lavoisier", endereco: "Av. Pref. Valdírio Prisco, 200 – Centro", lat: -23.7081625, lng: -46.4173833 },
+    { nome: "Meliponário – Abelhas da Justiça", endereco: "Av. Pref. Valdírio Prisco, 150 – Centro", lat: -23.7083724, lng: -46.4175207 },
+    { nome: "Meliponário – Colégio Gran Leone", endereco: "Jardim Panorama – R. Águida Tori Sortino, 90 – Centro", lat: -23.7075713, lng: -46.4087648 },
+    { nome: "Meliponário – Abelhas da Saúde", endereco: "Rua Júlio Prestes 22, Jardim Luzo", lat: -23.7112517, lng: -46.38577 },
+    { nome: "Meliponário – Pomar Urbano", endereco: "Estr. da Col., 1500 – Santa Luzia", lat: -23.6944009, lng: -46.3967345 },
+    { nome: "Meliponário – LeBem", endereco: "Av. Benjamim Baptista Cerezoli, 580 – Pilar Velho", lat: -23.6821879, lng: -46.4074776 },
+    { nome: "Meliponário – Abelhas da Educação", endereco: "R. Emerson Conde Soares Giacomini, 200 – Ouro Fino", lat: -23.6818131, lng: -46.355889 },
+    { nome: "Meliponário – CRI", endereco: "R. Alferes Botacin, 171 – Centro Alto", lat: -23.7157106, lng: -46.4132153 },
+    { nome: "Meliponário – Escola Municipal Edir Maria de Oliveira", endereco: "Av. Ver. Rubens Maziero, 526 – Sítio Santana", lat: -23.6747838, lng: -46.3452608 },
+    { nome: "Meliponário – Toth", endereco: "R. Osvaldo Cruz, 02 – Centro", lat: -23.71965, lng: -46.4105132 },
+    { nome: "Meliponário – Estancia Delfis", endereco: "R. Eugênio Galo, 145 – Centro", lat: -23.7067633, lng: -46.4084658 },
+    { nome: "Meliponário – CAPS Infantil", endereco: "R. Primeiro de Maio, 108 – Jardim Itacolomy", lat: -23.706735, lng:-46.4167056 }
 ];
+
 
 
 const map = L.map('map').setView([-23.7111, -46.4140], 13);
